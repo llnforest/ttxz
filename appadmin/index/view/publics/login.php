757e1986c92c1ff18971eb39bb330c88c7e90663
llnforest <1220343451@qq.com>
2018-05-29 14:51:10 +0800

    '修改'

--- appadmin/index/view/publics/login.php
+++ appadmin/index/view/publics/login.php
@@ -7,29 +7,35 @@
         -moz-background-size: cover;
         -o-background-size: cover;
         background-size: cover;
+        overflow:hidden;
     }
-    .m-login { width:340px;background-color:#fafafa; position:absolute; left:50%; top:50%; margin-left:-170px; margin-top:-120px; box-shadow: 0 0 10px #333;border-radius:5px; padding-bottom:15px; color:#666;}
-    .m-login .login-head { background-color:#f0f0f0; height:50px;line-height:50px; padding-left:10px; font-size:16px; border-bottom:1px solid #ebebeb; border-radius:5px 5px 0px 0px; text-align:center; color:#909090; text-shadow:#fff 1px 1px 0px;}
-    .form-input{width: 300px;margin: auto;padding-top: 10px; }
+    .m-login { width: 400px;height: 260px;padding: 30px;position: absolute;left: 0;margin: auto;top: 0;bottom: 0;right: 0;border-radius: 5px;border: 2px solid #009688;}
+    .m-login .login-head { height:40px;line-height:40px; padding-left:10px; font-size:24px;  text-align:center; color:#009688;margin-bottom:10px; font-weight: 700;}
+    .layui-form-item input:focus{
+        border-style:solid;
+        border-color: #009688;
+        box-shadow: 0 0 30px #009688;
+        outline: #009688;
+    }
+    .m-login .logo{margin-right:10px;}
+    canvas{width:100%;}
 </style>
+<div id="app"></div>
 <div class="m-login">
-    <div class="login-head">系统管理登录</div>
-    <form id="login-form" action="{:Url('publics/login')}" method="post" role="form">
-
-        <div  class="form-input">
-            <div class="form-group field-loginform-username required">
-
-                <input type="text" class="form-control" name="name" maxlength="256" placeholder="用户名" value="">
+    <div class="login-head"><img class="logo" src="__PublicAdmin__/images/logo.png">恒通科技管理系统</div>
+    <form id="login-form" class="layui-form" action="{:Url('publics/login')}" method="post" role="form">
+            <div class="layui-form-item field-loginform-username required">
+                <input type="text" class="layui-input" name="name" maxlength="256" placeholder="用户名" value="">
             </div>
-            <div class="form-group field-loginform-password required">
-                <input type="password" id="loginform-password" class="form-control" name="login_password" maxlength="256" placeholder="密码">
-
+            <div class="layui-form-item field-loginform-password required">
+                <input type="password" id="loginform-password" class="layui-input" name="login_password" maxlength="256" placeholder="密码">
             </div>
 
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary u-login-btn">登录</button>
+            <div class="layui-form-item">
+                <button type="submit" class="layui-btn layui-btn-fluid u-login-btn">登录</button>
             </div>
-        </div>
     </form>
 
 </div>
+<script src="__PublicAdmin__/js/plugins/login_canvas_1.js?{$Think.config.version_time}"></script>
+<script src="__PublicAdmin__/js/plugins/login_canvas_2.js?{$Think.config.version_time}"></script>
